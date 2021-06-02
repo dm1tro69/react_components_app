e5e04a0811132d544ad14cb882cd1d654658ed76
dm1tro69 <dimolg22@gmail.com>
2021-06-02 13:59:14 +0300

    add material ui

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,57 @@
+import { useState, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch("https://randomuser.me/api/?results=200");
+        const { results, error } = await response.json();
+        if (error) {
+          throw new Error(error);
+        }
+        setData(results);
+        setIsError(false);
+      } catch (e) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getContacts();
+  }, []);
+  return {
+    data,
+    isError,
+    isLoading,
+  };
+};
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const contacts = useContacts();
+
+  if (contacts.isLoading) {
+    return <div>...loading</div>;
+  }
+  if (contacts.isError) {
+    return <div>...error</div>;
+  }
+
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <div>Contacts {contacts.data[0].name.first}</div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
